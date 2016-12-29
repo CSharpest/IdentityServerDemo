19b34340c9d92f4e72397d7c725ccd553a2d6a8a
Joe Gannon <jgannon@dgdean.com>
2016-12-29 14:32:39 -0500

    client credentials happy path

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
-namespace IdentityServerCore
+namespace WebApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.Title = "IdentityServer";
-
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://localhost:5000") //from demo
+                .UseUrls("http://localhost:5001") //from demo
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
-           host.Run();
+            host.Run();
         }
     }
 }
